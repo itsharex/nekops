917136d76cef3284e10f35c61b608bf78623814f
Nya Candy <dev@candinya.com>
2025-04-02 22:09:55 +0800

    feat: add react-scan to analysis performance

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
+import { scan } from "react-scan";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Layout from "./Layout.tsx";
 
 import { store } from "@/store.ts";
 import { Provider } from "react-redux";
+
+scan({
+  enabled: true,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>

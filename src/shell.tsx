8e1d85a49e1018f353fa2325299d1fe5cb15c064
Nya Candy <dev@candinya.com>
2025-04-18 16:22:21 +0800

    chore: recover support for strict mode

--- src/shell.tsx
+++ src/shell.tsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createTheme, MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
@@ -19,16 +19,16 @@ const theme = createTheme({
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <MantineProvider theme={theme}>
-    <Notifications />
-    <ModalsProvider>
-      <WindowBackground />
+  <React.StrictMode>
+    <MantineProvider theme={theme}>
+      <Notifications />
+      <ModalsProvider>
+        <WindowBackground />
 
-      <TerminalProvider>
-        <ShellTabs />
-      </TerminalProvider>
-    </ModalsProvider>
-  </MantineProvider>,
-  // </React.StrictMode>,
+        <TerminalProvider>
+          <ShellTabs />
+        </TerminalProvider>
+      </ModalsProvider>
+    </MantineProvider>
+  </React.StrictMode>,
 );

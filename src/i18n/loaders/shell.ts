57c45189a3b4a0521e7bda6900f7e2027674cc36
Nya Candy <dev@candinya.com>
2025-05-04 14:52:44 +0800

    fix: import fallback lang from constants

--- src/i18n/loaders/shell.ts
+++ src/i18n/loaders/shell.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import detector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-import { supportedLngs } from "@/i18n/locales/constants.ts";
+import { fallbackLng, supportedLngs } from "@/i18n/locales/constants.ts";
 import { resources } from "@/i18n/locales/resources_shell.ts";
 
 i18n
@@ -11,7 +11,7 @@ i18n
   .init({
     resources,
     supportedLngs,
-    fallbackLng: "en-US",
+    fallbackLng,
     ns: ["shell"],
     defaultNS: "shell",
     interpolation: {
